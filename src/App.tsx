9944368591395b6dd026990b462bd562bb82305a
leskevich <leskevichtema@gmail.com>
2022-11-28 15:29:08 +0100

    dev merge commit2

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <div></div>
-     <div></div>
-     <div></div>
+     <div>1</div>
+     <div>2</div>
+     <div>3</div>
     </div>
   );
 }
